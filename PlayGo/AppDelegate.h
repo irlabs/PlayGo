9a2772f9385fa387bcfa621cbca890c57dc612e3
irlabs <developer@irlabs.nl>
2012-03-25 11:24:54 +0200

    Initial Commit

---
+++ PlayGo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PlayGo
+//
+//  Created by Dirk van Oosterbosch on 25-03-12.
+//  Copyright (c) 2012 IR labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PlayGo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PlayGo
+//
+//  Created by Dirk van Oosterbosch on 25-03-12.
+//  Copyright (c) 2012 IR labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
