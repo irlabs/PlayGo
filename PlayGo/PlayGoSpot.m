883287a9ad3d73d2c0cc7217858d78178bf2d374
irlabs <developer@irlabs.nl>
2012-03-25 15:04:12 +0200

    playing audio

---
+++ PlayGo/PlayGoSpot.m
@@ -0,0 +1,13 @@
+//
+//  PlayGoSpot.m
+//  PlayGo
+//
+//  Created by Dirk van Oosterbosch on 25-03-12.
+//  Copyright (c) 2012 IR labs. All rights reserved.
+//
+
+#import "PlayGoSpot.h"
+
+@implementation PlayGoSpot
+
+@end
